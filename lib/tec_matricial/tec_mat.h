84dd99481c9e9ea15600fda6554df42644686bb1
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-23 20:47:20 -0300

    intento libreria

--- lib/tec_matricial/tec_mat.h
+++ lib/tec_matricial/tec_mat.h
@@ -1,5 +1,5 @@
-#ifndef TEC_MAT
-#define TEC_MAT
+#ifndef TEC_MAT_H
+#define TEC_MAT_H
 
 void tec_mac_Init(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4);
 void tec_mac(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4);
