8484b0f5ffe9f8a110350f3cb525d371cc1908a6
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-23 21:39:23 -0300

    main.c

--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 #include "stm32f103xb.h"
 #include "tec_mat.h"
-
+char tecla;
 int main(void) {
     int teclado = 0;
     tec_mac_Init (1, 2, 3, 4, 5, 6, 7, 8);
     while (1) {
-        tec_mac(1, 2, 3, 4, 5, 6, 7, 8);
+        tecla = tec_mac(1, 2, 3, 4, 5, 6, 7, 8);
     }
     return 0;
 }
