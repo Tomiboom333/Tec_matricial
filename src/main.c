308be96f082b0a8e2fe1d76566054f5717dd184a
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-23 21:37:47 -0300

    cambio main.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,9 @@
 
 int main(void) {
     int teclado = 0;
-
-    void tec_mac_Init (1, 2, 3, 4, 5, 6, 7, 8);
+    tec_mac_Init (1, 2, 3, 4, 5, 6, 7, 8);
     while (1) {
-        char tec_mac(1, 2, 3, 4, 5, 6, 7, 8);
+        tec_mac(1, 2, 3, 4, 5, 6, 7, 8);
     }
     return 0;
 }
