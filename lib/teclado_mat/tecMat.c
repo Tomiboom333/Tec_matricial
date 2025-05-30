0b9d412d5bb49ee78657afb9692e20518f1e0585
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-30 14:42:15 -0300

    cambios

--- lib/teclado_mat/tecMat.c
+++ lib/teclado_mat/tecMat.c
@@ -10,40 +10,32 @@ char teclado[4][4]={
 
 void tecMat_Init(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4){
 
-    RCC -> APB2ENR|=RCC_APB2ENR_IOPAEN;
+    RCC -> APB2ENR|=RCC_APB2ENR_IOPAEN;//habilito clocks
 
-    GPIOA->CRL &= ~(0xF<<(f1* 4));    
-    GPIOA->CRL |=  (1 << (f1 * 4));//OUTPUT push pull;0001.
+    //configuracion de filas y columnas
+    int filas[4]={f1,f2,f3,f4};
+    int columnas[4]={c1,c2,c3,c4};
 
-    GPIOA->CRL &= ~(0xF<<(f2* 4));    
-    GPIOA->CRL |=  (1 << (f2 * 4));//OUTPUT push pull;0001.
+    for(int i = 0;i<4;i++){
+        if(columnas[i]<8){
+            GPIOA -> CRL &= ~(0xF<<(columnas[i]* 4));
+            GPIOA->CRL |=  (1 << ((columnas[i] * 4)+3));
+            GPIOA -> BSRR |= (1<<columnas[i]);
+        }else{
+            GPIOA -> CRH &= ~(0xF<<(columnas[i] % 8)*4);
+            GPIOA->CRH |=  (1 << (((columnas[i] % 8)*4)+3));
+            GPIOA -> BSRR |= (1<<columnas[i]);
+        }
 
-    GPIOA->CRL &= ~(0xF<<(f3* 4));    
-    GPIOA->CRL |=  (1 << (f3 * 4));//OUTPUT push pull;0001.
-
-    GPIOA->CRL &= ~(0xF<<(f4* 4));    
-    GPIOA->CRL |=  (1 << (f4 * 4));//OUTPUT push pull;0001.
-
-    GPIOA->CRL &= ~(0xF<<(c1* 4));   
-    GPIOA->CRL |=  (1 << ((c1* 4)+3));   // Cff = 10 , MODE = 00.(input pull up) */
-    GPIOA->BSRR|=(1<<(c1));
-
-    GPIOA->CRL &= ~(0xF<<(c2* 4));   
-    GPIOA->CRL |=  (1 << ((c2* 4)+3));   // Cff = 10 , MODE = 00.(input pull up) */
-    GPIOA->BSRR|=(1<<(c2));
-
-    GPIOA->CRL &= ~(0xF<<(c3* 4));   
-    GPIOA->CRL |=  (1 << ((c3* 4)+3));   // Cff = 10 , MODE = 00.(input pull up) */
-    GPIOA->BSRR|=(1<<(c3));
-
-    GPIOA->CRL &= ~(0xF<<(c4* 4));   
-    GPIOA->CRL |=  (1 << ((c4* 4)+3));   // Cff = 10 , MODE = 00.(input pull up) */
-    GPIOA->BSRR|=(1<<(c4));
-
-    GPIOA->BSRR|=(1<<(f1));
-    GPIOA->BSRR|=(1<<(f2));
-    GPIOA->BSRR|=(1<<(f3));
-    GPIOA->BSRR|=(1<<(f4));
+        if(filas[i]<8){
+            GPIOA -> CRL &= ~(0xF<<(filas[i]* 4));
+            GPIOA->CRL |=  (1 << (filas[i] * 4));
+        }else{
+            GPIOA -> CRH &= ~(0xF<<(filas[i] % 8)*4);
+            GPIOA->CRH |=  (1 << ((filas[i] % 8)*4));
+        }
+        GPIOA -> BSRR |= (1<<filas[i]);
+    }
 }
 
 char tecMat(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4){
@@ -51,14 +43,11 @@ char tecMat(int f1, int f2, int f3, int f4, int c1, int c2, int c3, int c4){
     int columnas[4]={c1,c2,c3,c4};
 
     for(int i=0;i<4;i++){
-        GPIOA->BSRR |= (1 << filas[0]) | (1 << filas[1]) | (1 << filas[2]) | (1 << filas[3]);
         GPIOA->BSRR|=(1<<(filas[i] + 16));
         for(int j=0;j<4;j++){
-             if ((GPIOA->IDR & (1 << columnas[j])) == 0) {
-                return teclado[i][j];
-             }
+            if (!(GPIOA->IDR & (1 << columnas[j]))) return teclado[i][j];
         }
-    GPIOA->BSRR |= (1 << filas[i]);
+        GPIOA->BSRR |= (1 << filas[i]);
     }
     return 0;
 }
