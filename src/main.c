1202c7f7adb5362b13b952aab62fa678c1b3196c
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-30 17:24:31 -0300

    Teclado

--- src/main.c
+++ src/main.c
@@ -3,11 +3,12 @@
 #include <string.h>
 #include "stdbool.h"
 char pwi[7]; //password ingresada
-char pwc[7]= "CB2008"; //password correcta
+char pwc[7]= {'1','1','1','1','1','1','\0'}; //password correcta
 char pwn[7]; //password nueva
 int ledV = 9, ledR = 10;
 int i=0;
 int pos=0;
+int checking=0;
 bool correcto=false, hash=false;
 int main(void) {
     tecMat_Init (1, 2, 3, 4, 5, 6, 7, 8);
@@ -21,11 +22,11 @@ int main(void) {
     while (1) { 
         char caract = tecMat(1, 2, 3, 4, 5, 6, 7, 8);
         if(caract!='x'){
+           
             if(hash){
-                if(pos<6){
                     pwn[pos] = caract;
                     pos++;
-                } else if(pos==6){
+                   if(pos==6){
                     pwn[6]='\0';
                     strcpy(pwc, pwn);
                     memset(pwn, 0, sizeof(pwn));
@@ -41,20 +42,22 @@ int main(void) {
                 }              
             }
             else{
-                pwi[i] = caract;
-                i++;
-                if(i<6){
+                pwi[pos++] = caract;
+                if(pos==6){
                     pwi[6]='\0';
-                    if (strcmp(pwi, pwc) == 0){
-                    GPIOA -> BSRR |= (1<<ledV);
-                    GPIOA -> BSRR |= (1<<ledR+16);
-                    correcto=true;
-                }else{
+                    for(int i=0;i<7;i++){
+                        if(pwi[i]==pwc[i]) checking++;
+                    }
+                    if (checking==7){
+                        GPIOA -> BSRR |= (1<<ledV);
+                        GPIOA -> BSRR |= (1<<(ledR+16));
+                        correcto=true;
+                    }else{
                     //apagar led y encender rojo 
-                    GPIOA -> BSRR |= (1<<ledR);
-                    GPIOA -> BSRR |= (1<<ledV+16);
-                    correcto=false;
-                }
+                        GPIOA -> BSRR |= (1<<ledR);
+                        GPIOA -> BSRR |= (1<<(ledV+16));
+                        correcto=false;
+                    }
             }
             //borro el string y reseteo posicion
             memset(pwi, 0, sizeof(pwi));
