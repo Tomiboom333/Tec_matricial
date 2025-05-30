8b6ccd2e7e9dc0f75ddf033f39adb703bff1a576
Tomiboom333 <tomasriquelme1239@gmail.com>
2025-05-30 15:17:20 -0300

    A ver si funca

--- src/main.c
+++ src/main.c
@@ -1,15 +1,27 @@
 #include "stm32f103xb.h"
 #include "tecMat.h"
-char pwi[6];
+#include <string.h>
+char pwi[7];
 char pwc[6]= "CB2008";
-int i=0;
+char pwn[7];
+int i;
 int main(void) {
     tecMat_Init (1, 2, 3, 4, 5, 6, 7, 8);
     while (1) {
+        i=0;
         while((tecMat(1, 2, 3, 4, 5, 6, 7, 8) != 'x') | (i<6)){
             pwi[i] = tecMat(1, 2, 3, 4, 5, 6, 7, 8);
             i++;
+            }
+        if (strcmp(pwi, pwc) == 0){
+            if (tecMat(1, 2, 3, 4, 5, 6, 7, 8) == '#'){
+                while((tecMat(1, 2, 3, 4, 5, 6, 7, 8) != 'x') | (i<6)){
+                    pwn[i] = tecMat(1, 2, 3, 4, 5, 6, 7, 8);
+                    i++;
+                }
+                strcpy(pwc, pwn);
+            }
+        }
     }
-    }
-    return 0;
+return 0;
 }
